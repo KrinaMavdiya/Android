b63110481245635b36abbe2cbc6937c9e131d9b7
KrinaMavdiya <66817230+KrinaMavdiya@users.noreply.github.com>
2021-11-12 22:29:21 +0530

    First commit
    login activity

---
+++ LoginActivity/app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.loginactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder res = new StringBuilder();
+                res.append("Loged in ");
+                Toast.makeText(MainActivity.this,res,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
